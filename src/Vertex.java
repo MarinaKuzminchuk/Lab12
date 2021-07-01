fbab0516514a5610f0450295e5a5fd11cdb2ac15
LeanderAK <82841550+LeanderAK@users.noreply.github.com>
2021-07-01 14:37:01 +0200

    Create Vertex.java

---
+++ src/Vertex.java
@@ -0,0 +1,16 @@
+
+public class Vertex {
+
+	String name;
+	
+	public void Vertex(){
+		
+	}
+	public void setName(String s) {
+		this.name = s;
+	}
+	public String getName() {
+		return this.name;
+	}
+
+}
